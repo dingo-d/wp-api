ae88e944e1e44f276af85122afbe83d7e11ac466
dingo-d <denis.zoljom@gmail.com>
2017-05-21 23:14:00 +0200

    Add data stores

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -1,8 +1,11 @@
-var React = require('react');
+import React from 'react';
+import DataStore from './../../stores/DataStore.js';
 
 class Home extends React.Component {
 
   render() {
+    let allData = DataStore.getAll();
+
     return (
       <div>
         <h1>Hello World</h1>
--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Home from './components/home/Home';
+import DataActions  from './actions/DataActions.js';
+
 require('./sass/application.scss');
 
 import {
@@ -12,12 +14,34 @@ import {
   Router
 } from 'react-router';
 
-ReactDOM.render((
-    <Router history={browserHistory}>
-      <Route path="/" component={ App } >
-        <IndexRoute component={ Home } />
-      </Route>
-      <Redirect from="*" to="/" />
-    </Router>
-  ), document.getElementById('app')
-);
+class InitializeApp {
+
+  buildRoutes(data) {
+    return data.pages.map((page, i) => {
+      return (
+        <Route
+          component={ Home }
+          key={ page.id }
+          path={`/${page.slug}`}
+        />
+      );
+    });
+  }
+
+  run() {
+    DataActions.getPages((response) => {
+      ReactDOM.render(
+        <Router history={browserHistory}>
+          <Route path="/" component={ App } >
+            <IndexRoute component={ Home } />
+            {this.buildRoutes(response)}
+          </Route>
+          <Redirect from="*" to="/" />
+        </Router>
+        , document.getElementById('app')
+      );
+    });
+  }
+}
+
+new InitializeApp().run();
